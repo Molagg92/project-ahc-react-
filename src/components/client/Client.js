73113b5153a13b8cf7402c801a36c4468f718c3c
Erik Z <molagg92@gmail.com>
2024-06-20 22:57:19 -0700

    make Client component, forgot to set up Firebase

---
+++ src/components/client/Client.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+function Client() {
+  const [clientList, setClientList] = useState([]);
+  const [newClientName,setNewClientName ] = useState("");
+
+  return(
+    <h2>Name</h2>
+  );
+}
+
+export default Client;
