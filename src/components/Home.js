f3fc67ab2db4d9be3ee7021d99e90f524f469034
Erik Z <molagg92@gmail.com>
2024-06-21 00:13:05 -0700

    add button to render Client page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from 'react';
+import Client from '../components/client/Client';
 
 function Home() {
+  const [currentPage, setCurrentPage] = useState('home');
+
+  const renderPage = () => {
+    if (currentPage === 'home') {
+      return (
+        <div>
+          <h1>Home Page!</h1>
+          <button onClick={() => setCurrentPage('client')}> Go To Client Page</button>
+        </div>
+      )
+    } else if (currentPage === 'client'){
+      return <Client />
+    }
+  }
   return (
-    <div>
-      <h1> Welcome to Ana's House Cleaning App!</h1>
-      <h4>We are currently under construction!</h4>
-    </div>
+    <>
+    {renderPage()}
+    </>
   );
 }
 
