b4ac17a382994aece2e21944173b5cabf1684723
Erik Z <molagg92@gmail.com>
2024-06-20 22:52:29 -0700

    make home page

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React from "react";
+import Home from "./Home";
 
 function App() {
   return (
    <>
-
-
+    <Home />
    </>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1> Welcome to Ana's House Cleaning App!</h1>
+      <h4>We are currently under construction!</h4>
+    </div>
+  );
+}
+
+export default Home;
