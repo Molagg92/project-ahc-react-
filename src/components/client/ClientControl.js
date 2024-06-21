6af7843483fc597cfc63396434af8d339e431a86
Erik Z <molagg92@gmail.com>
2024-06-21 11:27:28 -0700

    display the DB clientList in ClientControll

--- src/components/client/ClientControl.js
+++ src/components/client/ClientControl.js
@@ -1,8 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CreateClient from './CreateClient';
+import{ getDocs, collection } from 'firebase/firestore';
+import { db } from '../../config/firebase'
 
 function ClientControl({ navigateHome  }) {
   const [currentOperation, setCurrentOperation] = useState('control')
+  const [clientList, setClientList] = useState([]);
+
+  const clientCollectionRef = collection(db, 'client');
+
+  const getClientList = async () => {
+    try {
+      const data = await getDocs(clientCollectionRef);
+      const specifitData = data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setClientList(specifitData);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    getClientList();
+  }, []);
 
   const renderOperationPage = () => {
     if (currentOperation === 'create') {
@@ -13,6 +35,11 @@ function ClientControl({ navigateHome  }) {
         <h2> Client Control Page</h2>
         <button onClick={() => setCurrentOperation('create')}>Create Client</button>
         <button onClick={navigateHome}>Go Back to Home</button>
+        {clientList.map((client) => (
+          <div key={client.id}>
+          <p>{client.name}</p>
+          </div>
+      ))}
       </div>
     )
   }
--- src/components/client/CreateClient.js
+++ src/components/client/CreateClient.js
@@ -1,30 +1,10 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { db } from '../../config/firebase';
-import{ getDocs, collection, addDoc } from 'firebase/firestore';
+import{ collection, addDoc } from 'firebase/firestore';
 
 function CreateClient({goBack}) {
-  // const [clientList, setClientList] = useState([]);
   const [newClientName,setNewClientName ] = useState("");
-
-
-
-  // const getClientList = async () => {
-  //   try {
-  //     const data = await getDocs(clientCollectionRef);
-  //     const specifitData = data.docs.map((doc) => ({
-  //       ...doc.data(),
-  //       id: doc.id,
-  //     }));
-  //     setClientList(specifitData);
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   getClientList();
-  // }, []);
 
   const onClientSubmit = async () => {
     const clientCollectionRef = collection(db, "client");
@@ -40,22 +20,16 @@ function CreateClient({goBack}) {
 
   return(
   <div>
-   <div>
-    <h2>Create Client Page</h2>
-    <input
-      placeholder="Name?"
-      onChange={(e) => setNewClientName(e.target.value)}
-    />
-    <button onClick={onClientSubmit}> Submit Client </button>
-    <button onClick={goBack}>Go Back</button>
+    <div>
+      <h2>Create Client Page</h2>
+      <input
+        placeholder="Name?"
+        onChange={(e) => setNewClientName(e.target.value)}
+      />
+      <button onClick={onClientSubmit}> Submit Client </button>
+      <button onClick={goBack}>Go Back</button>
     </div>
-      {/* {clientList.map((client) => (
-        <div key={client.id}>
-          <p>{client.name}</p>
-        </div>
-      ))} */}
-    
-    </div>
+  </div>
     
   );
 }
